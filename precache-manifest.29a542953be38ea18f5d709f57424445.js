3b9c65d5a4de23d4a75a8acae77473f40fd0ed1d
Markus Kullberg <kullberg.markus@gmail.com>
2019-04-07 10:31:53 +0300

    Updates

---
+++ precache-manifest.29a542953be38ea18f5d709f57424445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2431edb597fd4e932074",
+    "url": "/web/static/js/runtime~main.f367b2fd.js"
+  },
+  {
+    "revision": "6d79df3fba0a02de973c",
+    "url": "/web/static/js/main.3f0565ec.chunk.js"
+  },
+  {
+    "revision": "f897c6422659fa0f5dc3",
+    "url": "/web/static/js/2.8356d4f2.chunk.js"
+  },
+  {
+    "revision": "6d79df3fba0a02de973c",
+    "url": "/web/static/css/main.c38bd2f1.chunk.css"
+  },
+  {
+    "revision": "85f268e658d09f611743cbfffc64c3ae",
+    "url": "/web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kullmar/web/precache-manifest.f4d5b7c4202ae1db1d9e68df7555ab86.js"
+  "/web/precache-manifest.29a542953be38ea18f5d709f57424445.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kullmar/web/index.html", {
+workbox.routing.registerNavigationRoute("/web/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
